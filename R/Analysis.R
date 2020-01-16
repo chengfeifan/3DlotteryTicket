3cfe76d954240d2b398e01034d92e6b9811f70f9
chengfeifan <chengff11@gmail.com>
2020-01-16 15:09:19 +0800

    add combine 6

--- R/Analysis.R
+++ R/Analysis.R
@@ -7,14 +7,15 @@ randomGen <- function(Date){
   return(random3D)
 }
 
-# Compare the random 
-CompareData <- function(Data){
+# random buy result
+# randomBuy(Data)
+randomBuy <- function(Data){
   dataRandom <- randomGen(Data$Date)
   data <- data.frame('Date'=Data$Date,'Data'=paste(Data$Num1,Data$Num2,Data$Num3,sep=''))
   dataAll <- data %>% left_join(dataRandom,by = 'Date')
   count <- 0
   for(i in 1:nrow(dataAll)){
-    if(as.numeric(dataAll$Data[i]) == as.numeric(dataAll$num3D[i]))
+    if(as.numeric(as.character(dataAll$Data[i])) == as.numeric(as.character(dataAll$num3D[i])))
       count <- count + 1
   }
   winRate <- count/nrow(dataAll)
@@ -31,3 +32,29 @@ print.winRate <- function(winRate){
   cat('您的中奖概率: ',winRate$winRate,'\n')
   cat('您的盈利率为',winRate$winMoney,'元')
 }
+
+# change string to character set
+str2chrSet <- function(stringObj){
+  strSet <- NULL
+  for(i in 1:nchar(stringObj)){
+    strSet <- c(strSet,substr(stringObj,i,i))
+  }
+  return(strSet)
+}
+# random buy combination
+randomBuyCom6 <- function(Data){
+  dataRandom <- randomGen(Data$Date)
+  data <- data.frame('Date'=Data$Date,'Data'=paste(Data$Num1,Data$Num2,Data$Num3,sep=''))
+  dataAll <- data %>% left_join(dataRandom,by = 'Date')
+  count <- 0
+  for(i in 1:nrow(dataAll)){
+    if(setequal(str2chrSet(as.character(dataAll$Data[i])), str2chrSet(as.character(dataAll$num3D[i]))))
+      count <- count + 1
+  }
+  winRate <- count/nrow(dataAll)
+  winMoney <- count*160 - 2 * nrow(dataAll)
+  result <- list('Date'=Data$Date, 'count'=count, 'winRate'=winRate, 'winMoney'=winMoney)
+  class(result) <- 'winRate'
+  return(result)
+}
+# randomBuyCom6(Data = Data)
