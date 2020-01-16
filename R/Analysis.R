30239f4b2fefef74118317c854c2aac5ad68b81b
chengfeifan <chengff11@gmail.com>
2020-01-16 16:01:05 +0800

    Revise the mistake of combination buy

--- R/Analysis.R
+++ R/Analysis.R
@@ -7,6 +7,7 @@ randomGen <- function(Date){
   return(random3D)
 }
 
+#############################################################################################
 # random buy result
 # randomBuy(Data)
 randomBuy <- function(Data){
@@ -19,7 +20,7 @@ randomBuy <- function(Data){
       count <- count + 1
   }
   winRate <- count/nrow(dataAll)
-  winMoney <- count*1000 - 2 * nrow(dataAll)
+  winMoney <- count*1040 - 2 * nrow(dataAll)
   result <- list('Date'=Data$Date, 'count'=count, 'winRate'=winRate, 'winMoney'=winMoney)
   class(result) <- 'winRate'
   return(result)
@@ -28,11 +29,12 @@ randomBuy <- function(Data){
 print.winRate <- function(winRate){
   n <- length(winRate$Date)
   cat('在过去的',n,'天里','\n')
-  cat('您的中将次数：',winRate$count,'\n')
+  cat('您的中奖次数：',winRate$count,'\n')
   cat('您的中奖概率: ',winRate$winRate,'\n')
   cat('您的盈利率为',winRate$winMoney,'元')
 }
 
+#####################################################################################
 # change string to character set
 str2chrSet <- function(stringObj){
   strSet <- NULL
@@ -41,20 +43,40 @@ str2chrSet <- function(stringObj){
   }
   return(strSet)
 }
-# random buy combination
-randomBuyCom6 <- function(Data){
+# random buy combination 
+randomBuyCom <- function(Data){
   dataRandom <- randomGen(Data$Date)
   data <- data.frame('Date'=Data$Date,'Data'=paste(Data$Num1,Data$Num2,Data$Num3,sep=''))
   dataAll <- data %>% left_join(dataRandom,by = 'Date')
-  count <- 0
+  count6 <- 0
+  count3 <- 0
   for(i in 1:nrow(dataAll)){
-    if(setequal(str2chrSet(as.character(dataAll$Data[i])), str2chrSet(as.character(dataAll$num3D[i]))))
-      count <- count + 1
+    data1 <- str2chrSet(as.character(dataAll$Data[i]))
+    data2 <- str2chrSet(as.character(dataAll$num3D[i]))
+    # 因为c(1,1,2)和c(1,2,2)在set上是相等的,所以要判断两个组合是否完全一样
+    
+    if(setequal(data1, data2) & length(table(data1)) == 3 & length(table(data2)) == 3)
+      count6 <- count6 + 1
+    if(setequal(data1, data2) & length(table(data1)) == 2 & length(table(data2)) == 2 ){
+      if(sum(table(data1) == table(data2)) == 2){
+        count3 <- count3 + 1
+      }
+    }
   }
-  winRate <- count/nrow(dataAll)
-  winMoney <- count*160 - 2 * nrow(dataAll)
-  result <- list('Date'=Data$Date, 'count'=count, 'winRate'=winRate, 'winMoney'=winMoney)
-  class(result) <- 'winRate'
+  winRate <- (count3 + count6)/nrow(dataAll)
+  winMoney <- count6*173 + count3*346 - 2 * nrow(dataAll)
+  result <- list('Date'=Data$Date, 'count3'=count3, 'count6'=count6, 'winRate'=winRate, 'winMoney'=winMoney)
+  class(result) <- 'winRateCom'
   return(result)
 }
-# randomBuyCom6(Data = Data)
+# randomBuyCom(Data = Data)
+
+print.winRateCom <- function(winRate){
+  n <- length(winRate$Date)
+  cat('在过去的',n,'天里','\n')
+  cat('您的中组选3次数：',winRate$count3,'\n')
+  cat('您的中组选6次数：',winRate$count6,'\n')
+  cat('您的中奖概率: ',winRate$winRate,'\n')
+  cat('您的盈利率为',winRate$winMoney,'元')
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # 3DlotteryTicket
-To predict 3D lottery ticket
+To predict 3D lottery ticket. 
+
+## 中文简介
+用于测试购买3D福利彩票，其中`randomBuy`是买直选,`randomBuyCom`是买组选。
+
+```
+library(dplyr)
+library(readxl)
+library(ggplot2)
+library(lubridate)
+
+data(Data)
+randomBuy(Data)
+randomBuyCom(Data)
+```
+
+
